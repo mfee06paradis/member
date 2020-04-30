0e9f2f98b4ad3331f02d868bebce94c99c9178bd
matt94nanari <wind.traveler@hotmail.com>
2020-04-30 10:16:44 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React from 'react';
-import Cart from './pages/Cart';
-import ProductList from './pages/ProductList';
-import Product from './pages/Product';
-import Uielements from './pages/Uielements';
-import Member from './pages/Member';
 
-import Menu from './components/Menu';
-import Footer from './components/Footer';
+import Member from './pages/Member';
 
 function App() {
   return (
     <Router>
       <>
-        <Menu />
-        <Switch>
-          <Route path="/Uielements">
-            <Uielements />
-          </Route>
-          <Route path="/cart">
-            <Cart />
-          </Route>
-          <Route path="/product">
-            <Product />
-          </Route>
-          <Route exact path="/">
-            <ProductList />
-          </Route>
-        </Switch>
         <Member />
-        <Footer />
       </>
     </Router>
   );
--- src/pages/Member.js
+++ src/pages/Member.js
@@ -5,7 +5,72 @@ import Top from '../components/Top';
 function Member() {
   return (
     <>
-      <SideBar />
+      <div className="row">
+        <SideBar />
+        <div className="col mainCotent">
+          <div className="col-12 memberdec">
+            <img src={require('../images/memberdec.svg')} alt="memberdec" />
+          </div>
+          <img
+            className="registerBox2"
+            src={require('../images/registerbox2.svg')}
+            alt="registerBox2"
+          />
+          <div className="col-12">
+            <img src={require('../images/member-icon.svg')} alt="membericon" />
+          </div>
+          <div className="col-12">
+            <img src={require('../images/pink-ribbon.svg')} alt="pinkribbon" />
+          </div>
+          <div className="col-12">
+            <img
+              className="registerBox"
+              src={require('../images/registerbox.svg')}
+              alt="registerbox"
+            />
+          </div>
+          <div className="row">
+            <div className="col-6">
+              <img
+                src={require('../images/account2.svg')}
+                alt="account2"
+                className="mobileIcon"
+              />
+            </div>
+            <div className="col-6">
+              <img
+                src={require('../images/creditCard2.svg')}
+                alt="creditCard2"
+                className="mobileIcon"
+              />
+            </div>
+            <div className="col-6">
+              <img
+                src={require('../images/myorder2.svg')}
+                alt="myorder2"
+                className="mobileIcon"
+              />
+            </div>
+            <div className="col-6">
+              <img
+                src={require('../images/myfavorite2.svg')}
+                alt="myfavorite2"
+                className="mobileIcon"
+              />
+            </div>
+            <div className="col-2"></div>
+            <div className="col-8">
+              <img
+                src={require('../images/logout.svg')}
+                alt="logout"
+                className="mobileIcon"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="col-3 side-bar"></div>
+      </div>
       <Top />
     </>
   );
